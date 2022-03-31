5428b6239f110b8add826d1e66168643d067b8e7
shuvo-99 <anontoshuvo99@gmail.com>
2022-03-31 13:04:01 +0600

    heading text done

---
+++ src/components/heading/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h2 class="headinghtext1">Buy your game</h2>
+      <h2 class="headinghtext2">Select any 4</h2>
+    </div>
+  );
+};
+
+export default Header;
