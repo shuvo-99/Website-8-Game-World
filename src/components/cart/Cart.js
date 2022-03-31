9ca92c72e276be160ad3e6d6a1f1378e943843c8
shuvo-99 <anontoshuvo99@gmail.com>
2022-03-31 23:27:19 +0600

    main task done

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,32 +5,43 @@ import "./Cart.css";
 const Cart = (props) => {
   const { cart } = props;
   // console.log(cart);
-
-  let i = "";
-
+  let i = [];
   for (const product of cart) {
-    // i = i + <br /> + product.name;
-    i.map(product.name);
+    i.push(product.name);
+    if (i.length == 5) {
+      alert("You cannot add any more than 4");
+    }
+  }
+
+  console.log(i);
+  let a = "";
+
+  function chooseRandom() {
+    const random = Math.floor(Math.random() * i.length);
+    console.log(random, i[random]);
+    if (i[random] != undefined) {
+      document.getElementById("show").value = i[random];
+    }
   }
 
   return (
     <div className="cart">
-      <h2>Selected Clothes</h2>
+      <h2>Selected Games</h2>
       {cart.map((product) => (
-        <div className="">
-          <img src={product.img} alt="" />
-          <p>{product.name}</p>
+        <div class="cart-select">
+          <img src={product.image} alt="" />
+          <p class="cart-select-text">{product.name}</p>
         </div>
       ))}
+
+      <button onClick={chooseRandom}>Choose 1 for me</button>
+
+      <div>
+        <input id="show" />
+      </div>
+      {/* <button onClick={reset}>Choose again</button> */}
     </div>
   );
 };
 
 export default Cart;
-
-// let j = "";
-// for (let a = 0; a < i.length; a++) {
-//   j = i[a];
-//   <br />;
-// }
-// console.log(j);
